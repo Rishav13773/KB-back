7cdaab087a9c6502483973ffc65eb3f02f7525a9
Rishav <ahoul72@gmail.com>
2024-02-04 12:00:03 +0530

    trash api

--- controllers/project.js
+++ controllers/project.js
@@ -41,19 +41,30 @@ exports.getprojectbyid = (req, res) => {
   // console.log("start");
   try {
     const uid = req.params.id;
-    console.log("uid : ", uid);
+    const state = req.query.state;
+    // console.log(state);
     const user = User.findById(uid);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
     // console.log("project Id: ", req.params )
-    Project.find({ createdBy: uid })
-      .then((projects) => {
-        res.send(projects);
-      })
-      .catch((error) => {
-        res.status(500).json({ message: error.message });
-      });
+    if (state) {
+      Project.find({ createdBy: uid, trash: state })
+        .then((projects) => {
+          res.send(projects);
+        })
+        .catch((error) => {
+          res.status(500).json({ message: error.message });
+        });
+    } else {
+      Project.find({ createdBy: uid, trash: state })
+        .then((projects) => {
+          res.send(projects);
+        })
+        .catch((error) => {
+          res.status(500).json({ message: error.message });
+        });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -74,6 +85,59 @@ exports.getdocumentByPid = (req, res) => {
         res.status(500).json({ message: error.message });
       });
   } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.trashProject = async (req, res) => {
+  try {
+    const pid = req.params.id;
+    const state = req.body.state;
+
+    const project = await Project.findByIdAndUpdate(pid, { trash: state });
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    console.log("reached");
+    res.json({ message: "Project trashed", project });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.restoreProject = async (req, res) => {
+  try {
+    const pid = req.params.id;
+    const state = req.body.state;
+
+    const project = await Project.findByIdAndUpdate(pid, { trash: state });
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    console.log("reached");
+    res.json({ message: "Project trashed", project });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deletProject = async (req, res) => {
+  console.log("reached");
+  try {
+    const pid = req.params.id;
+    console.log("Deleting project with ID:", pid);
+    const project = await Project.findByIdAndDelete(pid);
+    console.log("Deleted project:", project);
+
+    res.json({ message: "Project deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting project:", error.message);
     res.status(500).json({ message: error.message });
   }
 };
